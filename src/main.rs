3d5d6e3fb2a589dbc131301889915f9241ad639a
shinjung <tlswjd9015@gmail.com>
2025-01-24 16:53:58 +0900

    초간단 정수계산기

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
     println!("Hello, world!");
+    // let value = serde_json::json!({
+    //     "code": 200,
+    //     "success": true,
+    //     "payload": {
+    //         "features": [
+    //             "serde",
+    //             "json"
+    //         ]
+    //     }
+    // });
+    //
+    // println!("json = {:?}", value);
+
+    // 간단한 정수 계산기
+    let program = "+ + * - /";
+    let mut accumulator = 5;
+
+    for token in program.chars() {
+        match token {
+            '+' => accumulator += 1,
+            '-' => accumulator -= 1,
+            '*' => accumulator *= 2,
+            '/' => accumulator /= 2,
+            _ => { /* 다른 문자들 무시 */ }
+        }
+    }
+
+    println!("프로그램 \"{}\"의 계산값은 {}입니다.", program, accumulator);
 }
---
+++ src/practice1.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // casting = simple type change using 'as'
+    let my_number = 'a' as u8;
+    println!("my_number = {}", my_number);
+}
